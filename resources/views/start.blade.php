a6431ec42279ee9083b8a44be19e3b4b088491a7
Gabriel Erick <gabrielmmve13@gmail.com>
2025-05-16 00:17:36 -0300

    arrumando váriaveis padrão e criando tela inicial

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     <title>Spotify</title>
 </head>
-<body>
-    <div class="container bg-body">
-        <img src="{{ asset('assets/images/logo.png') }}" alt="Logo do Spotify">
-    </div>
+
+<style>
+    .logo {
+        width: 50vw;
+    }
+</style>
+
+{{-- min-vh-100 pois assim é garantido que a tela usará pelo menos 100% da altura, independente do elemento pai (h-100 pega 100% do elemento pai, vh pega da tela) --}}
+<body class="bg-body d-flex flex-column align-items-center justify-content-center min-vh-100">
+
+    <img class="logo" src="{{ asset('assets/images/logo.png') }}" alt="Logo do Spotify">
+
 </body>
+
 </html>
