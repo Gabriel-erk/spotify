33d9f3960b486836850895e48b082683dce87d29
Gabriel Erick <gabrielmmve13@gmail.com>
2025-05-17 18:51:10 -0300

    configurações iniciais

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,12 @@
 <x-layout title="Spotify - Home">
+    <header class="d-flex align-items-center">
+        <img id="user" class="rounded-circle" src="{{ asset('assets/images/eu.jpg') }}" alt="Imagem do usuário">
 
+        <ul class="d-flex gap-3 list-unstyled"> 
+            <li><a href="#" class="text-decoration-none">Tudo</a></li>
+            <li><a href="#" class="text-decoration-none">Música</a></li>
+            <li><a href="#" class="text-decoration-none">Podcast</a></li>
+        </ul>
+
+    </header>
 </x-layout>
