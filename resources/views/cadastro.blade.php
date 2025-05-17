3f40f3a5768b753eda22303300e655d6576b8369
Gabriel Erick <gabrielmmve13@gmail.com>
2025-05-17 13:44:54 -0300

    adicionando componentes

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -1,45 +1,32 @@
-<!DOCTYPE html>
-<html lang="pt_BR">
+<x-layout title="Spotify - Cadastro">
+    <div class="d-flex justify-content-center my-5">
+        <img src="{{ asset('assets/images/logo.png') }}" alt="Logo do Spotify" style="width: 50vw">
+    </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    <title>Spotify - Cadastro</title>
-</head>
+    <div>
+        <form action="">
+            <div class="mb-4">
+                <input type="text" class="form-control bg-secondary fw-medium border border-0" id="nome"
+                    placeholder="Digite o seu nome">
+            </div>
 
-<body class="bg-body">
-    <main class="container py-5">
-        <div class="d-flex justify-content-center mb-5">
-            <img src="{{ asset('assets/images/logo.png') }}" alt="Logo do Spotify" style="width: 50vw">
-        </div>
+            <div class="mb-4">
+                <input type="email" class="form-control bg-secondary fw-medium  border border-0" id="email"
+                    placeholder="Digite o seu e-mail">
+            </div>
 
-        <div>
-            <form action="">
-                <div class="mb-4">
-                    <input type="text" class="form-control bg-secondary fw-medium border border-0" id="nome" placeholder="Digite o seu nome" >
-                </div>
+            <div class="mb-4">
+                <input type="password" id="password" class="form-control bg-secondary fw-medium  border border-0"
+                    aria-describedby="passwordHelpBlock" placeholder="*********">
+            </div>
 
-                <div class="mb-4">
-                    <input type="email" class="form-control bg-secondary fw-medium  border border-0" id="email"
-                        placeholder="Digite o seu e-mail">
-                </div>
+            <div class="mb-4">
+                <button class="btn btn-primary fw-bold w-100">Cadastrar</button>
+            </div>
 
-                <div class="mb-4">
-                    <input type="password" id="password" class="form-control bg-secondary fw-medium  border border-0" aria-describedby="passwordHelpBlock" placeholder="*********">
-                </div>
-
-                <div class="mb-4">
-                    <button class="btn btn-primary fw-bold w-100">Cadastrar</button>
-                </div>
-
-            </form>
-            <a href="#" class="text-decoration-none">
-                <p class="text-white fw-bold text-center fw-medium ">Já possui login? Faça o login</p>
-            </a>
-        </div>
-    </main>
-</body>
-
-</html>
+        </form>
+        <a href="#" class="text-decoration-none">
+            <p class="text-white fw-bold text-center fw-medium ">Já possui login? Faça o login</p>
+        </a>
+    </div>
+</x-layout>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,41 +1,27 @@
-<!DOCTYPE html>
-<html lang="pt_BR">
+<x-layout title="Spotify - Login">
+    <div class="d-flex justify-content-center my-5">
+        <img src="{{ asset('assets/images/logo.png') }}" alt="Logo do Spotify" style="width: 50vw">
+    </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    <title>Spotify - Login</title>
-</head>
+    <div>
+        <form action="">
+            <div class="mb-4">
+                <input type="email" class="form-control bg-secondary fw-medium  border border-0" id="email"
+                    placeholder="Digite o seu e-mail">
+            </div>
 
-<body class="bg-body">
-    <main class="container py-5">
-        <div class="d-flex justify-content-center mb-5">
-            <img src="{{ asset('assets/images/logo.png') }}" alt="Logo do Spotify" style="width: 50vw">
-        </div>
+            <div class="mb-4">
+                <input type="password" id="password" class="form-control bg-secondary fw-medium  border border-0"
+                    aria-describedby="passwordHelpBlock" placeholder="*********">
+            </div>
 
-        <div>
-            <form action="">
-                <div class="mb-4">
-                    <input type="email" class="form-control bg-secondary fw-medium  border border-0" id="email"
-                        placeholder="Digite o seu e-mail">
-                </div>
+            <div class="mb-4">
+                <button class="btn btn-primary fw-bold w-100">Acessar</button>
+            </div>
 
-                <div class="mb-4">
-                    <input type="password" id="password" class="form-control bg-secondary fw-medium  border border-0" aria-describedby="passwordHelpBlock" placeholder="*********">
-                </div>
-
-                <div class="mb-4">
-                    <button class="btn btn-primary fw-bold w-100">Acessar</button>
-                </div>
-
-            </form>
-            <a href="#" class="text-decoration-none">
-                <p class="text-white fw-bold text-center fw-medium ">Ainda não possui conta? Cadastre-se</p>
-            </a>
-        </div>
-    </main>
-</body>
-
-</html>
+        </form>
+        <a href="#" class="text-decoration-none">
+            <p class="text-white fw-bold text-center fw-medium ">Ainda não possui conta? Cadastre-se</p>
+        </a>
+    </div>
+</x-layout>
