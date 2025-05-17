c89ce3ab204f209013170ba595d1f01dbee2898b
Gabriel Erick <gabrielmmve13@gmail.com>
2025-05-17 19:19:14 -0300

    começando criação da home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,10 @@
 <x-layout title="Spotify - Home">
-    <header class="d-flex align-items-center">
+    <header class="d-flex align-items-center gap-2 p-4">
         <img id="user" class="rounded-circle" src="{{ asset('assets/images/eu.jpg') }}" alt="Imagem do usuário">
 
-        <ul class="d-flex gap-3 list-unstyled"> 
-            <li><a href="#" class="text-decoration-none">Tudo</a></li>
-            <li><a href="#" class="text-decoration-none">Música</a></li>
-            <li><a href="#" class="text-decoration-none">Podcast</a></li>
+        <ul class="d-flex align-items-center list-unstyled">
+            <li><button type="button" class="btn btn-outline-primary rounded-pill">Tudo</button></li>
+           
         </ul>
 
     </header>
