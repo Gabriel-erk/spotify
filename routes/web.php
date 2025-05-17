a63ecaf2398f85a33835a1282a3a54deb39eec6d
Gabriel Erick <gabrielmmve13@gmail.com>
2025-05-17 18:32:44 -0300

    iniciando configuração da home

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+})->name('home');
+
+Route::get('/cadastro', function () {
     return view('cadastro');
 })->name('cadastro');
 
